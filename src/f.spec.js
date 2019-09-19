cc39be8f3449abae5779e2767e4f3ad2790311aa
Matias Grunberg <matias@yellowspot.dev>
2019-09-19 14:43:27 -0300

    Write one spec

--- src/f.spec.js
+++ src/f.spec.js
@@ -1,9 +1,10 @@
-var assert = require('assert');
+const { expect } = require('chai');
+
 describe('Basic Mocha String Test', function () {
- it('should return number of charachters in a string', function () {
-        assert.equal("Hello".length, 4);
-    });
- it('should return first charachter of the string', function () {
-        assert.equal("Hello".charAt(0), 'H');
-    });
+  it('should return number of charachters in a string', function () {
+    expect("Hello").to.have.length(5)
+  });
+  it('should return first charachter of the string', function () {
+    expect("Hello".charAt(0)).to.equal('H');
+  });
 });
